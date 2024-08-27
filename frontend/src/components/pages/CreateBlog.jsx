96518f2ac13ab2f815ef026b0c8353beedfd94c6
Sahajaddin4 <sahajaddinsk786@gmail.com>
2024-08-27 19:53:14 +0530

    frontend and backend basic part done

--- frontend/src/components/pages/CreateBlog.jsx
+++ frontend/src/components/pages/CreateBlog.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 function CreateBlog() {
+
+  const [blogData,setBlogData]=useState({
+    title:"",
+    body:"",
+    author:""
+  })
+
+  function handleChange(e){
+     const{name,value}=e.target;
+     setBlogData(prevData=>{
+      return {
+        ...prevData,
+        [name]:value
+      }
+     })
+  }
   return (
     <div className="login w-[30em] mt-20 h-[20em] mx-auto rounded-lg shadow-2xl border px-4 py-2 m-2">
       <div className="heading text-center mb-5">
@@ -10,13 +26,14 @@ function CreateBlog() {
         <form className="flex flex-col gap-5">
           {/* Email input field */}
           <div className="title flex gap-2 items-center">
-            <label htmlFor="email">Title: </label>
+            <label htmlFor="#title">Title: </label>
             <input
               type="text"
               name="title"
               id="title"
-              // onChange={handleChange}
-              // value={userData.email}
+
+              onChange={handleChange}
+             value={blogData.title}
               className="border-2 ml-2 hover:border-blue-400 py-2 rounded w-full"
             />
           </div>
@@ -26,11 +43,13 @@ function CreateBlog() {
             <label htmlFor="body">Body:</label>
             <div className="relative border-2 hover:border-blue-400 py-2 rounded w-full">
               {/* Password input field */}
-              <input
-                // type={passwordType}
+              <textarea
+                value={blogData.body}
                 name="body"
                 id="body"
-                // value={userData.password}
+                minLength={500}
+                onChange={handleChange}
+              
                 className="w-full border-none outline-none pr-10"
               />
 
@@ -45,10 +64,12 @@ function CreateBlog() {
             <div className="relative border-2 hover:border-blue-400 py-2 rounded w-full">
               {/* Password input field */}
               <input
-                // type={passwordType}
+              value={blogData.author}
+               type="text"
                 name="author"
                 id="author"
-                // value={userData.password}
+                
+                onChange={handleChange}
                 className="w-full border-none outline-none pr-10"
               />
 
